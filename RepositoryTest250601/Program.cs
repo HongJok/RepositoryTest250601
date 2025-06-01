d3c4656f71329b384f4e2368f75939532de844a7
Hong Jok <jdok6177@gmail.com>
2025-06-01 23:01:02 +0900

    Add project files.

---
+++ RepositoryTest250601/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryTest250601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi JD 250601?");
+            Console.ReadLine();
+        }
+    }
+}
